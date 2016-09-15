100d1b6f509a144b18840c6af02db9d65c33cc1e
tanelson1991 <tanelson1991@gmail.com>
2016-09-15 13:21:37 -0400

    Assignment 1
    This is what I've got for assignment 1 so far

---
+++ cmsc401.java
@@ -0,0 +1,32 @@
+/* Taylor Nelson
+ * Assignment 1: Inversion Vector
+ */
+package assignment1;
+
+import java.util.Scanner;
+
+public class cmsc401 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println();
+		
+		Scanner input = new Scanner(System.in);
+		int totalNums = input.nextInt();
+		
+		int[] W = new int[totalNums];
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		input.close();
+		
+	}
+
+}
